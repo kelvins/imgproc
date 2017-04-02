97717a43053e885c12a5f010ae0b6939f503b354
Kelvin S. do Prado <kelvinpfw@hotmail.com>
2017-04-02 20:01:32 -0300

    Applied go fmt

--- imgproc.go
+++ imgproc.go
@@ -1,153 +1,152 @@
-
 package imgproc
 
 import (
-    "os"
-    "errors"
-    "image"
-    "image/png"
-    _ "image/jpeg"
-    _ "image/png"
-    _ "image/gif"
-    "github.com/disintegration/imaging"
+	"errors"
+	"github.com/disintegration/imaging"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
+	_ "image/png"
+	"os"
 )
 
 // Check if an image is in grayscale
-func IsGrayscale(img image.Image) (bool) {
-    // Gets the width and height of the image
-    bounds := img.Bounds()
-    w, h := bounds.Max.X, bounds.Max.Y
+func IsGrayscale(img image.Image) bool {
+	// Gets the width and height of the image
+	bounds := img.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
 
-    if w == 0 || h == 0 {
-        return false
-    }
+	if w == 0 || h == 0 {
+		return false
+	}
 
-    // Verifies each pixel (R,G,B)
-    for x := 0; x < w; x++ {
-        for y := 0; y < h; y++ {
-            r, g, b, _ := img.At(x, y).RGBA()
-            if r != g && g != b {
-                return false
-            }
-        }
-    }
+	// Verifies each pixel (R,G,B)
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			if r != g && g != b {
+				return false
+			}
+		}
+	}
 
-    return true
+	return true
 }
 
 // Convert an image to grayscale
-func ToGrayscale(img image.Image) (image.Image) {
-      // Get the image bounds
-      bounds := img.Bounds()
-      w, h := bounds.Max.X, bounds.Max.Y
-      gray := image.NewGray(bounds)
+func ToGrayscale(img image.Image) image.Image {
+	// Get the image bounds
+	bounds := img.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+	gray := image.NewGray(bounds)
 
-      // Convert each pixel to grayscale
-      for x := 0; x < w; x++ {
-          for y := 0; y < h; y++ {
-              gray.Set(x, y, img.At(x, y))
-          }
-      }
+	// Convert each pixel to grayscale
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			gray.Set(x, y, img.At(x, y))
+		}
+	}
 
-      // Return the new image in grayscale
-      return gray
+	// Return the new image in grayscale
+	return gray
 }
 
 // Save an image (Image) to an image file
-func SaveImage(img image.Image, filePath string) (error) {
-    // Creates the file based on the file path
-    out, err := os.Create(filePath)
+func SaveImage(img image.Image, filePath string) error {
+	// Creates the file based on the file path
+	out, err := os.Create(filePath)
 
-    // Writes the image (img) to out in PNG format
-    err = png.Encode(out, img)
+	// Writes the image (img) to out in PNG format
+	err = png.Encode(out, img)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 // Convert the image to grayscale and then convert it to a integer slice
-func ConvertImageToVector(img image.Image) ([]uint8) {
+func ConvertImageToVector(img image.Image) []uint8 {
 
-    var imageSlice []uint8
+	var imageSlice []uint8
 
-    // Get the image bounds
-    bounds := img.Bounds()
-    w, h := bounds.Max.X, bounds.Max.Y
-    gray := image.NewGray(bounds)
+	// Get the image bounds
+	bounds := img.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+	gray := image.NewGray(bounds)
 
-    // Convert each pixel to grayscale
-    for x := 0; x < w; x++ {
-        for y := 0; y < h; y++ {
-            gray.Set(x, y, img.At(x, y))
-            imageSlice = append(imageSlice, gray.GrayAt(x, y).Y)
-        }
-    }
+	// Convert each pixel to grayscale
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			gray.Set(x, y, img.At(x, y))
+			imageSlice = append(imageSlice, gray.GrayAt(x, y).Y)
+		}
+	}
 
-    return imageSlice
+	return imageSlice
 }
 
 // Load an image file based on the file path
 func LoadImage(filePath string) (image.Image, error) {
-      // Open the file image
-      fImage, err := os.Open(filePath)
+	// Open the file image
+	fImage, err := os.Open(filePath)
 
-      if err != nil {
-          return nil, err
-      }
+	if err != nil {
+		return nil, err
+	}
 
-      // Ensure that the image file will be closed
-      defer fImage.Close()
+	// Ensure that the image file will be closed
+	defer fImage.Close()
 
-      // Convert it to an image "object"
-      img, _, err := image.Decode(fImage)
+	// Convert it to an image "object"
+	img, _, err := image.Decode(fImage)
 
-      if err != nil {
-          return nil, err
-      }
+	if err != nil {
+		return nil, err
+	}
 
-      return img, nil
+	return img, nil
 }
 
 // Load multiple images based on a slice of image paths
 func LoadMultipleImages(paths []string) ([]image.Image, error) {
 
-  // If the paths slice is empty
-  if len(paths) == 0 {
-    return nil, errors.New("The slice is empty")
-  }
+	// If the paths slice is empty
+	if len(paths) == 0 {
+		return nil, errors.New("The slice is empty")
+	}
 
-  // Slice that will store the images loaded
-  var images []image.Image
+	// Slice that will store the images loaded
+	var images []image.Image
 
-  // For each image path tries to load the image
-  for index := 0; index < len(paths); index++ {
-    // Try to load the image
-    image, err := LoadImage(paths[index])
+	// For each image path tries to load the image
+	for index := 0; index < len(paths); index++ {
+		// Try to load the image
+		image, err := LoadImage(paths[index])
 
-    // If something went wrong return an error
-    if err != nil {
-      return nil, err
-    }
+		// If something went wrong return an error
+		if err != nil {
+			return nil, err
+		}
 
-    // Append the image loaded to the images slice
-    images = append(images, image)
-  }
+		// Append the image loaded to the images slice
+		images = append(images, image)
+	}
 
-  // Return the images slice
-  return images, nil
+	// Return the images slice
+	return images, nil
 }
 
 // Resize the image based on the parameters (maximum size 10.000x10.000)
 func ResizeImage(img image.Image, width int, height int) (image.Image, error) {
-    if width == 0 || width > 10000 {
-        return nil, errors.New("Invalid width")
-    }
-    if height == 0 || height > 10000 {
-        return nil, errors.New("Invalid height")
-    }
+	if width == 0 || width > 10000 {
+		return nil, errors.New("Invalid width")
+	}
+	if height == 0 || height > 10000 {
+		return nil, errors.New("Invalid height")
+	}
 
-    return imaging.Resize(img, width, height, imaging.Lanczos), nil
+	return imaging.Resize(img, width, height, imaging.Lanczos), nil
 }
--- imgproc_test.go
+++ imgproc_test.go
@@ -1,179 +1,178 @@
-
 package imgproc_test
 
 import (
-    "testing"
-    "github.com/kelvins/imgproc"
+	"github.com/kelvins/imgproc"
+	"testing"
 )
 
 // Test the load image function
 func TestLoadImage(t *testing.T) {
-    // Try to load an invalid image
-    _, err := imgproc.LoadImage("123")
+	// Try to load an invalid image
+	_, err := imgproc.LoadImage("123")
 
-    if err == nil {
-        t.Error(err)
-    }
+	if err == nil {
+		t.Error(err)
+	}
 
-    // Try to load an valid image
-    _, err = imgproc.LoadImage("./face.png")
+	// Try to load an valid image
+	_, err = imgproc.LoadImage("./face.png")
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 // Test the load multiple images function
 func TestLoadMultipleImages(t *testing.T) {
-  var paths []string
+	var paths []string
 
-  // Paths empty
-  _, err := imgproc.LoadMultipleImages(paths)
+	// Paths empty
+	_, err := imgproc.LoadMultipleImages(paths)
 
-  if err == nil {
-      t.Error("Expected an error. The paths slice is empty.")
-  }
+	if err == nil {
+		t.Error("Expected an error. The paths slice is empty.")
+	}
 
-  // Invalid path
-  paths = append(paths, "123.txt")
-  images, err := imgproc.LoadMultipleImages(paths)
+	// Invalid path
+	paths = append(paths, "123.txt")
+	images, err := imgproc.LoadMultipleImages(paths)
 
-  if images != nil {
-      t.Error("Expected images == nil")
-  }
-  if err == nil {
-      t.Error("Expected an error. The path is invalid.")
-  }
+	if images != nil {
+		t.Error("Expected images == nil")
+	}
+	if err == nil {
+		t.Error("Expected an error. The path is invalid.")
+	}
 
-  // Valid paths
-  paths = nil
-  paths = append(paths, "./face.png")
-  paths = append(paths, "./face.png")
+	// Valid paths
+	paths = nil
+	paths = append(paths, "./face.png")
+	paths = append(paths, "./face.png")
 
-  images, err = imgproc.LoadMultipleImages(paths)
+	images, err = imgproc.LoadMultipleImages(paths)
 
-  if images == nil {
-      t.Error("Expected a valid images vector")
-  }
-  if err != nil {
-      t.Error("Expected no error.")
-  }
+	if images == nil {
+		t.Error("Expected a valid images vector")
+	}
+	if err != nil {
+		t.Error("Expected no error.")
+	}
 }
 
 // Test the SaveImage function
 func TestSaveImage(t *testing.T) {
 
-    img, err := imgproc.LoadImage("./face.png")
+	img, err := imgproc.LoadImage("./face.png")
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 
-    err = imgproc.SaveImage(img, "./face.png")
+	err = imgproc.SaveImage(img, "./face.png")
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 // Test the grayscale function
 func TestGrayscale(t *testing.T) {
 
-    img, err := imgproc.LoadImage("./face.png")
+	img, err := imgproc.LoadImage("./face.png")
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 
-    if imgproc.IsGrayscale(img) {
-        t.Error("Image is grayscale")
-    }
+	if imgproc.IsGrayscale(img) {
+		t.Error("Image is grayscale")
+	}
 
-    newImage := imgproc.ToGrayscale(img)
+	newImage := imgproc.ToGrayscale(img)
 
-    if !imgproc.IsGrayscale(newImage) {
-        t.Error("Image is not grayscale")
-    }
+	if !imgproc.IsGrayscale(newImage) {
+		t.Error("Image is not grayscale")
+	}
 }
 
 // Test the ConvertImageToVector function
 func TestConvertImageToVector(t *testing.T) {
 
-    img, err := imgproc.LoadImage("./face.png")
+	img, err := imgproc.LoadImage("./face.png")
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 
-    imageSlice := imgproc.ConvertImageToVector(img)
+	imageSlice := imgproc.ConvertImageToVector(img)
 
-    expectedImageSlice := [6]uint8{163, 163, 162, 160, 157, 157}
+	expectedImageSlice := [6]uint8{163, 163, 162, 160, 157, 157}
 
-    for i := 0; i < len(expectedImageSlice); i++ {
-        if imageSlice[i] != expectedImageSlice[i] {
-            t.Error(err)
-        }
-    }
+	for i := 0; i < len(expectedImageSlice); i++ {
+		if imageSlice[i] != expectedImageSlice[i] {
+			t.Error(err)
+		}
+	}
 
-    if len(imageSlice) != (256*256) {
-        t.Error("Wrong length")
-    }
+	if len(imageSlice) != (256 * 256) {
+		t.Error("Wrong length")
+	}
 }
 
 // Test the resize image function
 func TestResizeImage(t *testing.T) {
-    img, err := imgproc.LoadImage("./face.png")
+	img, err := imgproc.LoadImage("./face.png")
 
-    if err != nil {
-        t.Error(err)
-    }
+	if err != nil {
+		t.Error(err)
+	}
 
-    b := img.Bounds()
-    w, h := b.Max.X, b.Max.Y
+	b := img.Bounds()
+	w, h := b.Max.X, b.Max.Y
 
-    if w != 256 || h != 256 {
-        t.Error("Wrong bounds")
-    }
+	if w != 256 || h != 256 {
+		t.Error("Wrong bounds")
+	}
 
-    imgResize, err := imgproc.ResizeImage(img, 100, 0)
+	imgResize, err := imgproc.ResizeImage(img, 100, 0)
 
-    if err == nil {
-        t.Error("Error during the resize image")
-    }
+	if err == nil {
+		t.Error("Error during the resize image")
+	}
 
-    imgResize, err = imgproc.ResizeImage(img, 0, 100)
+	imgResize, err = imgproc.ResizeImage(img, 0, 100)
 
-    if err == nil {
-        t.Error("Error during the resize image")
-    }
+	if err == nil {
+		t.Error("Error during the resize image")
+	}
 
-    imgResize, err = imgproc.ResizeImage(img, 0, 0)
+	imgResize, err = imgproc.ResizeImage(img, 0, 0)
 
-    if err == nil {
-        t.Error("Error during the resize image")
-    }
+	if err == nil {
+		t.Error("Error during the resize image")
+	}
 
-    imgResize, err = imgproc.ResizeImage(img, 9999999, 9999999)
+	imgResize, err = imgproc.ResizeImage(img, 9999999, 9999999)
 
-    if err == nil {
-        t.Error("Error during the resize image")
-    }
+	if err == nil {
+		t.Error("Error during the resize image")
+	}
 
-    imgResize, err = imgproc.ResizeImage(img, 100, 100)
+	imgResize, err = imgproc.ResizeImage(img, 100, 100)
 
-    b = imgResize.Bounds()
-    w, h = b.Max.X, b.Max.Y
+	b = imgResize.Bounds()
+	w, h = b.Max.X, b.Max.Y
 
-    if w != 100 || h != 100 {
-        t.Error("Wrong bounds")
-    }
+	if w != 100 || h != 100 {
+		t.Error("Wrong bounds")
+	}
 
-    imgResize, err = imgproc.ResizeImage(img, 10, 10)
+	imgResize, err = imgproc.ResizeImage(img, 10, 10)
 
-    b = imgResize.Bounds()
-    w, h = b.Max.X, b.Max.Y
+	b = imgResize.Bounds()
+	w, h = b.Max.X, b.Max.Y
 
-    if w != 10 || h != 10 {
-        t.Error("Wrong bounds")
-    }
+	if w != 10 || h != 10 {
+		t.Error("Wrong bounds")
+	}
 }
